efeaaa0bdcaaec995ccb00655871ff3d737b4c01
nicokinazoko <38060004+nicokinazoko@users.noreply.github.com>
2020-08-23 19:05:58 +0700

    Update Webpack

--- app.js
+++
@@ -1,4 +0,0 @@
-import main from './src/script/view/main.js';
-import "./src/script/component/app-bar.js";
-
-document.addEventListener("DOMContentLoaded", main);
---
+++ src/app.js
@@ -0,0 +1,5 @@
+import main from './script/view/main.js';
+import "./script/component/app-bar.js";
+import "./styles/style.css";
+import "regenerator-runtime";
+document.addEventListener("DOMContentLoaded", main);
